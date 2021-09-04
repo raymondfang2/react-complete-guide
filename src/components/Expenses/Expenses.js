867d5e0f54596015d0d7b87608024c0bdd5896e0
Fang Limin <fanglimin@Fangs-MacBook-Air.local>
2021-09-04 15:58:47 +0800

    ms

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import ExpenseList from "./ExpenseList";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseList items={filteredExpenses}></ExpenseList>
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
